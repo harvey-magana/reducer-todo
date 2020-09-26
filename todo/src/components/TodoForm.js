012afbe6b4c410ab6867c380355b362418765562
harvey magana <heyharv2012@gmail.com>
2020-09-25 20:35:38 -0700

    Updated the reducer with some starter code.

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,12 +1,20 @@
 import React, { useReducer } from 'react';
 import '../App.css';
-import { initialTodoState } from '../reducer/todoReducer';
+import { initialTodoState, todoReducer } from '../reducer/todoReducer';
 
 const TodoForm = () => {
-    const [ state, dispatch ] = useReducer(reducer, initialTodoState);
+    const [ state, dispatch ] = useReducer(initialTodoState, todoReducer);
     return (
         <div>
             TodoForm
+            <form>
+                <input 
+                    type="text"
+                    todo="item"
+                    value="some value"
+                />
+                <button>Add</button>
+            </form>
         </div>
     )
 }
--- todo/src/reducer/todoReducer.js
+++ todo/src/reducer/todoReducer.js
@@ -1,12 +1,28 @@
 export const initialTodoState = {
-    id: new Date(),
-    item: 'Learn about reducers',
-    completed: false
+    todos: [{
+        id: Date.now(),
+        item: 'Learn about reducers',
+        completed: false
+    }]
   }
 
 export const todoReducer = (state, action) => {
     switch(action.type) {
-        case "ADD_TODO": {}
+        case "ADD_TODO": {
+            {
+                const newTodo = {
+                    id: Date.now(),
+                    item: action.text,
+                    completed: false
+                }
+                return {
+                    todos: {
+                        ...state.todos, 
+                        newTodo
+                    }
+                }
+            }
+        }
         case "COMPLETE_TODO": {}
         case "REMOVE_TODO": {}
         default: 
