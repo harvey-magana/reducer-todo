8abaf73ade9c88b6c4e04d5b4bc97d00d610bc02
harvey magana <heyharv2012@gmail.com>
2020-09-27 15:47:03 -0700

    Cleaned up the TodoApp component.

--- todo/src/components/TodoApp.js
+++ todo/src/components/TodoApp.js
@@ -1,12 +1,8 @@
-import React, { useState, useReducer } from "react";
-//import TodoList from "./TodoList";
+import React from "react";
 import TodoForm from "./TodoForm";
-//import { todoReducer, initialTodoState, ACTIONS } from '../reducer/todoReducer';
 import '../App.css';
 
 const TodoApp = () => {
-  //const [ todos, dispatch ] = useReducer(todoReducer, [])
-
   return (
     <div className="container">
         <TodoForm />
